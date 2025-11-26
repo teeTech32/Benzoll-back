17c4e2c35b390b02bf6d609a89365ce452bdf25d
ATANDA TIMOTHY <timothyabidemi4618@gmail.com>
2025-11-26 16:35:06 +0100

    db.js

--- config/db.js
+++ config/db.js
@@ -3,8 +3,26 @@ const mongoose = require('mongoose')
 const connectDB = async() =>{
   try{
     const conn = await mongoose.connect(process.env.MONGO_URL,{
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      const mongoose = require('mongoose')
+
+const connectDB = async() =>{
+  try{
+    const conn = await mongoose.connect(process.env.MONGO_URL,{
+      maxPoolSize: 20,     // maximum number of connections in the pool
+      minPoolSize: 5,      // starting number of connections
+      serverSelectionTimeoutMS: 5000,
+    })
+    console.log(`Mongoose Connected: ${conn.connection.host}`.blue.underline.bold)
+
+  }catch(error){
+    console.log(`Error: ${error.message}`.red.underline.bold)
+    process.exit(1)
+    
+  }
+}
+module.exports = connectDB;
+
+
     })
     console.log(`Mongoose Connected: ${conn.connection.host}`.blue.underline.bold)
 
