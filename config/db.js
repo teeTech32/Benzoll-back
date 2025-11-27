02f767f616fba4863cae2c947019d6092ec3a5b6
ATANDA TIMOTHY <timothyabidemi4618@gmail.com>
2025-11-27 16:26:28 +0100

    db.js

--- config/db.js
+++ config/db.js
@@ -3,11 +3,6 @@ const mongoose = require('mongoose')
 const connectDB = async() =>{
   try{
     const conn = await mongoose.connect(process.env.MONGO_URL,{
-      const mongoose = require('mongoose')
-
-const connectDB = async() =>{
-  try{
-    const conn = await mongoose.connect(process.env.MONGO_URL,{
       maxPoolSize: 20,  
       minPoolSize: 5, 
       serverSelectionTimeoutMS: 5000,
@@ -22,15 +17,4 @@ const connectDB = async() =>{
 }
 module.exports = connectDB;
 
-
-    })
-    console.log(`Mongoose Connected: ${conn.connection.host}`.blue.underline.bold)
-
-  }catch(error){
-    console.log(`Error: ${error.message}`.red.underline.bold)
-    process.exit(1)
-    
-  }
-}
-module.exports = connectDB;
 
